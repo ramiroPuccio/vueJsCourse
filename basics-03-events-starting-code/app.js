4a35327ffadd36746269661fff1be57742ae7045
ramiroPuccio <ramiropuccio@gmail.com>
2023-07-19 17:29:59 -0300

    03-finish

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,9 +1,28 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      name:'',
+      counter: 14,
+      confirmedName:'',
     };
   },
+  methods:{
+    add(value){
+      this.counter += value;
+    },
+    reduce(value){
+      this.counter -= value;
+    },
+    setName(event, lastName){
+      this.name = event.target.value + ' ' + lastName;
+    },
+    confirmName(){
+      this.confirmedName =  this.name;
+    },
+    submitForm(event){
+      alert(event.target)
+    },
+  }
 });
 
 app.mount('#events');
