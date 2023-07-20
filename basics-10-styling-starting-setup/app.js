14f5c5de0a62a733da64fc651a79de8b1765bb3e
ramiroPuccio <ramiropuccio@gmail.com>
2023-07-20 18:33:54 -0300

    style-01

---
+++ basics-10-styling-starting-setup/app.js
@@ -0,0 +1,22 @@
+const app =  Vue.createApp({
+    data(){
+        return{
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false,
+        };
+    },
+    methods:{
+        selectBox(box){
+            if(box === 'A') this.boxASelected = true;
+            if(box === 'B') this.boxBSelected = true;
+            if(box === 'C') this.boxCSelected = true;
+        },
+        unSelectBox(box){
+            if(box === 'A') this.boxASelected = false;
+            if(box === 'B') this.boxBSelected = false;
+            if(box === 'C') this.boxCSelected = false;
+        }
+    }
+});
+app.mount('#styling');
