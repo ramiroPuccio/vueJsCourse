53df18547adb817bb529d25db5269e671d481aa1
ramiroPuccio <ramiropuccio@gmail.com>
2023-08-08 22:27:49 -0300

    componenets with cli starting

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <h2>My friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+</template>
+<script>
+export default {
+    data() {
+        return{
+            friends: [
+                {
+                    id:'manuel',
+                    name:'Manuel Lorenz',
+                    phone:'3516791866',
+                    email:'manu@mailfalso.com',
+                },
+                {
+                    id:'juan',
+                    name:'Januel Jorenz',
+                    phone:'351671231866',
+                    email:'janu@mailfalso.com',
+                },
+            ],
+        }
+    },
+}
+</script>
